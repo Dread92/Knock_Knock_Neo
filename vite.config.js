6ce077a1b9f9caea1152e4807ef92867a5356b65
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-06-18 05:28:58 +0300

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    proxy: {},
-    fs: {
-      strict: true,
-      allow: [],
-    },
-    hmr: {
-      protocol: '',
-      host: '',
-    },
-    mimeTypes: {
-      'text/javascript': ['js', 'jsx'],
-      'text/jsx': ['jsx'],
-    },
-  },
-});
+})
